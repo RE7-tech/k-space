10be337eaaae5536216e2e91cfece2016965b078
Thibault Barillet <93664560+tbarillet@users.noreply.github.com>
2024-01-15 12:20:52 +0100

    Update config.js file with environment variables

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,11 @@
 const config = {
     api: {
-        url: process.env.NEXT_PUBLIC_API_URL ?? 'https://360.api.re7.tech',
+        url: process.env.NEXT_PUBLIC_API_URL || 'https://360.api.re7.tech',
     },
     app: {
-        websiteUrl: process.env.NEXT_PUBLIC_WEBSITE_URL ?? 'https://klian.fr',
-        subscriptionUrl: process.env.NEXT_PUBLIC_SUBSCRIPTION_URL ?? 'https://souscription.klian.fr',
-        maintenanceMode: process.env.NEXT_PUBLIC_MAINTENANCE_MODE ?? false,
+        websiteUrl: process.env.NEXT_PUBLIC_WEBSITE_URL || 'https://klian.fr',
+        subscriptionUrl: process.env.NEXT_PUBLIC_SUBSCRIPTION_URL || 'https://souscription.klian.fr',
+        maintenanceMode: process.env.NEXT_PUBLIC_MAINTENANCE_MODE || false,
     },
     breakpoints: {
         sm: 640,
@@ -14,12 +14,12 @@ const config = {
         xl: 1280,
     },
     infos: {
-        prettyPhone: process.env.NEXT_PUBLIC_PRETTY_PHONE ?? '02 78 84 84 84',
-        rawPhone: process.env.NEXT_PUBLIC_RAW_PHONE ?? '0278848484',
-        facebookPageUrl: process.env.NEXT_PUBLIC_FB_ID ?? 'klianfr',
-        whatsappUrl: process.env.NEXT_PUBLIC_WA_NBR ?? 'https://wa.me/278848484',
-        email: process.env.NEXT_PUBLIC_CONTACT_EMAIL ?? 'contact@klian.fr',
-        certimatUrl: process.env.NEXT_PUBLIC_CERTIMAT_URL ?? 'https://certimat.fr/prescripteurs?partner=2240'
+        prettyPhone: process.env.NEXT_PUBLIC_PRETTY_PHONE || '02 78 84 84 84',
+        rawPhone: process.env.NEXT_PUBLIC_RAW_PHONE || '0278848484',
+        facebookPageUrl: process.env.NEXT_PUBLIC_FB_ID || 'klianfr',
+        whatsappUrl: process.env.NEXT_PUBLIC_WA_NBR || 'https://wa.me/278848484',
+        email: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'contact@klian.fr',
+        certimatUrl: process.env.NEXT_PUBLIC_CERTIMAT_URL || 'https://certimat.fr/prescripteurs?partner=2240'
     },
 }
 
