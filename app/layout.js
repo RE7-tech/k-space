636b6185da8d814e791d50536601481a06a813d5
Thibault Barillet <93664560+tbarillet@users.noreply.github.com>
2024-02-26 11:46:19 +0100

    Add @highlight-run/next package and initialize HighlightRun in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
 import { Figtree } from 'next/font/google';
 import './globals.css';
 import React from 'react';
-import DesktopLayout from '@/components/DesktopLayout';
 import { DrawerProvider } from '@/context/DrawerContext';
 import { AuthProvider } from '@/context/AuthContext';
 import { CustomerProvider } from '@/context/CustomerContext';
-import config from '@/utils/config';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { HighlightInit } from '@highlight-run/next/client'
 
 const inter = Figtree({
   weights: [400, 500, 600, 700, 900],
@@ -22,29 +19,42 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" sizes='any' />
-        <link rel="apple-touch-icon" href="/apple-touch-icon-iphone-60x60.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-ipad-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-iphone-retina-120x120.png"  />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-ipad-retina-152x152.png" />
+    <>
+      <HighlightInit
+				projectId={'jgovwv6d'}
+				serviceName="my-nextjs-frontend"
+				tracingOrigins
+				networkRecording={{
+					enabled: true,
+					recordHeadersAndBody: true,
+					urlBlocklist: [],
+				}}
+			/>
 
-      </head>
-      <body className={`${inter.className} overflow-hidden`}>
+      <html lang="fr">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          <link rel="icon" href="/favicon.ico" sizes='any' />
+          <link rel="apple-touch-icon" href="/apple-touch-icon-iphone-60x60.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-ipad-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-iphone-retina-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-ipad-retina-152x152.png" />
 
-        <AuthProvider>
-          <CustomerProvider>
-            <DrawerProvider>
-              {children}
-            </DrawerProvider>
-          </CustomerProvider>
-        </AuthProvider>
-      </body>
-    </html>
+        </head>
+        <body className={`${inter.className} overflow-hidden`}>
+
+          <AuthProvider>
+            <CustomerProvider>
+              <DrawerProvider>
+                {children}
+              </DrawerProvider>
+            </CustomerProvider>
+          </AuthProvider>
+        </body>
+      </html>
+    </>
   );
 }
