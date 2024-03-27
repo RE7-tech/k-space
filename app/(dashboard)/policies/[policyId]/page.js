d310179ee0fba1ae6909bb9d0b45c1316763b30f
Thibault Barillet <axiocontactezmoi@protonmail.com>
2024-03-27 17:15:39 +0100

    update

--- app/(dashboard)/policies/[policyId]/page.js
+++ app/(dashboard)/policies/[policyId]/page.js
@@ -127,155 +127,163 @@ export default function Policy({params}) {
                 {name: 'Mon contrat', href: '/policies/' + policyId},
             ]}/>
 
+            {policy?.status?.toLowerCase() !== "pending" ? <>
 
-            {(policy.is_unpaid ?? false) ? <Alert variant="danger">
-                Le paiement de votre assurance n'a pas pu être effectué. Veuillez mettre à jour votre moyen de paiement.
-            </Alert> : null}
-
-            {hasPendingTermination() ? <Alert variant="info">
-                <FontAwesomeIcon icon={faWarning} width={24} height={24} className="me-4"/>
-                Demande de résiliation en cours
-            </Alert> : null}
-
-            <div className="flex flex-col gap-4">
-                {hasDeclinedTermination() ? <Alert variant="danger">
-                    <div className="flex flex-col items-start gap-4">
-                        <p>
-                            <FontAwesomeIcon icon={faWarning} width={24} height={24} className="me-4"/>
-                            Votre demande de résiliation a été refusée.
-                        </p>
-                        {getLastTerminationDeclined()?.customer_comment ? <p className="text-gray-500 text-sm">
-                            {getLastTerminationDeclined()?.customer_comment}
-                        </p> : null}
-                    </div>
+                {(policy.is_unpaid ?? false) ? <Alert variant="danger">
+                    Le paiement de votre assurance n'a pas pu être effectué. Veuillez mettre à jour votre moyen de paiement.
                 </Alert> : null}
 
-                {getPolicyBalance() < -5 ? <Alert variant="info" clickable={true} onClick={() => {
-                    router.push('/regulariser/' + policyId);
-                }}>
-                    <div className={'flex flex-col gap-1'}>
-                        <div className="flex  items-start items-center text-lg font-semibold">
-                            <FontAwesomeIcon icon={faExclamationTriangle} width={24} height={24} className="me-4"/>
-                            Paiement en attente
+                {hasPendingTermination() ? <Alert variant="info">
+                    <FontAwesomeIcon icon={faWarning} width={24} height={24} className="me-4"/>
+                    Demande de résiliation en cours
+                </Alert> : null}
+
+                <div className="flex flex-col gap-4">
+                    {hasDeclinedTermination() ? <Alert variant="danger">
+                        <div className="flex flex-col items-start gap-4">
+                            <p>
+                                <FontAwesomeIcon icon={faWarning} width={24} height={24} className="me-4"/>
+                                Votre demande de résiliation a été refusée.
+                            </p>
+                            {getLastTerminationDeclined()?.customer_comment ? <p className="text-gray-500 text-sm">
+                                {getLastTerminationDeclined()?.customer_comment}
+                            </p> : null}
                         </div>
-                        <p>
-                            Votre prochaine échéance est en attente de paiement ({ Math.abs(getPolicyBalance()).toFixed(2) }€). Cliquez ici pour régulariser votre
-                            situation <FontAwesomeIcon icon={faChevronRight} width={18} height={18} className=""/>
-                        </p>
-                    </div>
-                </Alert> : null}
-            </div>
+                    </Alert> : null}
 
-            <div className="flex flex-row justify-between items-center">
-                <h1 className="text-[22px] font-bold mt-8 mb-8 font-poppins">
-                    {policy?.summary?.primary} - {policy?.summary?.secondary}
-                </h1>
+                    {getPolicyBalance() < -5 ? <Alert variant="info" clickable={true} onClick={() => {
+                        router.push('/regulariser/' + policyId);
+                    }}>
+                        <div className={'flex flex-col gap-1'}>
+                            <div className="flex  items-start items-center text-lg font-semibold">
+                                <FontAwesomeIcon icon={faExclamationTriangle} width={24} height={24} className="me-4"/>
+                                Paiement en attente
+                            </div>
+                            <p>
+                                Votre prochaine échéance est en attente de paiement ({ Math.abs(getPolicyBalance()).toFixed(2) }€). Cliquez ici pour régulariser votre
+                                situation <FontAwesomeIcon icon={faChevronRight} width={18} height={18} className=""/>
+                            </p>
+                        </div>
+                    </Alert> : null}
+                </div>
 
-                {(!policy.has_termination ?? true) ? <>
-                    <div>
-                        <div className="relative w-full">
-                            <FontAwesomeIcon icon={faEllipsisVertical} className="text-gray-400 cursor-pointer"
-                                             width={24} height={24} onClick={() => {
-                                // open menu
-                                setShowMenu(!showMenu);
-                            }}/>
+                <div className="flex flex-row justify-between items-center">
+                    <h1 className="text-[22px] font-bold mt-8 mb-8 font-poppins">
+                        {policy?.summary?.primary} - {policy?.summary?.secondary}
+                    </h1>
 
-                            {showMenu ? <div
-                                className="absolute top-8 right-0 bg-white shadow-lg rounded-lg p-4 w-[250px] z-50">
-                                <div className="flex flex-col gap-2 w-full">
+                    {(!policy.has_termination ?? true) ? <>
+                        <div>
+                            <div className="relative w-full">
+                                <FontAwesomeIcon icon={faEllipsisVertical} className="text-gray-400 cursor-pointer"
+                                                 width={24} height={24} onClick={() => {
+                                    // open menu
+                                    setShowMenu(!showMenu);
+                                }}/>
 
-                                    <div className="flex flex-row items-center gap-2 cursor-pointer hover:text-gray-800"
-                                         onClick={() => {
-                                             router.push('/policies/' + policyId + '/resiliation');
-                                         }}>
-                                        <FontAwesomeIcon icon={faBan} width={24} height={24} className="text-pink-500"/>
-                                        <span>
-                                            Résilier mon contrat
-                                        </span>
+                                {showMenu ? <div
+                                    className="absolute top-8 right-0 bg-white shadow-lg rounded-lg p-4 w-[250px] z-50">
+                                    <div className="flex flex-col gap-2 w-full">
+
+                                        <div className="flex flex-row items-center gap-2 cursor-pointer hover:text-gray-800"
+                                             onClick={() => {
+                                                 router.push('/policies/' + policyId + '/resiliation');
+                                             }}>
+                                            <FontAwesomeIcon icon={faBan} width={24} height={24} className="text-pink-500"/>
+                                            <span>
+                                                Résilier mon contrat
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div> : null}
+                            </div>
+                        </div>
+                    </> : null}
+                </div>
+
+                {/* Two columns in desktop mode, and one column in mobile mode */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <Card sizeMode={`adaptive`} className="w-full">
+
+                        <div className="flex flex-col justify-center items-center gap-1 w-full">
+                            <img src={productIcons[policy?.product?.type]?.src} width={64} height={64} className="mb-4"
+                                 alt="" priority/>
+
+                            <h3 className="text-xl font-bold font-poppins font-semibold">
+                                {policy?.summary?.primary}
+                            </h3>
+                            <p className="text-gray-500 text-lg">
+                                {policy?.summary?.secondary}
+                            </p>
+                            <div className="flex flex-row justify-around w-full gap-2 mt-6">
+
+                                <div className="flex flex-col items-center gap-2">
+                                    <div
+                                        className="rounded-full border-2 border-blue-800 p-4 w-[38px] h-[38px] flex items-center justify-center bg-gray-100">
+                                        <FontAwesomeIcon icon={faShield} width={32} height={32} className="text-blue-800"/>
+                                    </div>
+                                    <div className="text-md text-blue-default font-semibold opacity-60">
+                                        {ucfirst(policy?.formula?.name ?? '...')}
                                     </div>
                                 </div>
-                            </div> : null}
+
+                                <div className="flex flex-col items-center gap-2">
+                                    <div
+                                        className="rounded-full border-2 border-blue-800 p-4 w-[38px] h-[38px] flex items-center justify-center bg-gray-100">
+                                        <FontAwesomeIcon icon={faMoneyBill} width={32} height={32}
+                                                         className="text-blue-800"/>
+                                    </div>
+                                    <div className="text-md text-blue-default font-semibold opacity-60">
+                                        {formatEuro(policy?.total_premium ?? 0)}
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col items-center gap-2">
+                                    <div
+                                        className="rounded-full border-2 border-blue-800 p-4 w-[38px] h-[38px] flex items-center justify-center bg-gray-100">
+                                        <FontAwesomeIcon icon={faCalendar} width={32} height={32}
+                                                         className="text-blue-800"/>
+                                    </div>
+                                    <div className="text-md text-blue-default font-semibold opacity-60">
+                                        {formatDate(policy?.start_date)}
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
+
+                        {/* <div className="mt-8 text-center">
+                            <a href="#" className="text-gray-500 underline underline-offset-[3px]">
+                                Qu'est-ce qui est couvert par mon contrat ?
+                            </a>
+                        </div> */}
+
+
+                    </Card>
+
+                    <div className="flex flex-col gap-8 items-start justify-start w-full p-4 flex-1">
+                        {(quickLinks ?? []).map((quickLink, idx) => {
+                            return <div onClick={() => handleQuickLinkClick(quickLink)} key={idx}
+                                        className={`pb-5 w-full flex items-center gap-2 justify-between text-2xl ${idx === quickLinks.length - 1 ? '' : 'border-b-2  border-gray-200'} hover:opacity-80 cursor-pointer`}>
+                                <div className="text-lg flex justify-center items-center gap-4 font-medium">
+                                    <FontAwesomeIcon icon={quickLink.icon} width={32} height={32} className=""/>
+                                    <span>
+                                        {quickLink.name}
+                                    </span>
+                                </div>
+                                <FontAwesomeIcon icon={faChevronRight} width={18} height={18} className="text-gray-400"/>
+                            </div>
+                        })}
                     </div>
-                </> : null}
-            </div>
-
-
-            {/* Two columns in desktop mode, and one column in mobile mode */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                <Card sizeMode={`adaptive`} className="w-full">
-
-                    <div className="flex flex-col justify-center items-center gap-1 w-full">
-                        <img src={productIcons[policy?.product?.type]?.src} width={64} height={64} className="mb-4"
-                             alt="" priority/>
-
-                        <h3 className="text-xl font-bold font-poppins font-semibold">
-                            {policy?.summary?.primary}
-                        </h3>
-                        <p className="text-gray-500 text-lg">
-                            {policy?.summary?.secondary}
-                        </p>
-                        <div className="flex flex-row justify-around w-full gap-2 mt-6">
-
-                            <div className="flex flex-col items-center gap-2">
-                                <div
-                                    className="rounded-full border-2 border-blue-800 p-4 w-[38px] h-[38px] flex items-center justify-center bg-gray-100">
-                                    <FontAwesomeIcon icon={faShield} width={32} height={32} className="text-blue-800"/>
-                                </div>
-                                <div className="text-md text-blue-default font-semibold opacity-60">
-                                    {ucfirst(policy?.formula?.name ?? '...')}
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center gap-2">
-                                <div
-                                    className="rounded-full border-2 border-blue-800 p-4 w-[38px] h-[38px] flex items-center justify-center bg-gray-100">
-                                    <FontAwesomeIcon icon={faMoneyBill} width={32} height={32}
-                                                     className="text-blue-800"/>
-                                </div>
-                                <div className="text-md text-blue-default font-semibold opacity-60">
-                                    {formatEuro(policy?.total_premium ?? 0)}
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center gap-2">
-                                <div
-                                    className="rounded-full border-2 border-blue-800 p-4 w-[38px] h-[38px] flex items-center justify-center bg-gray-100">
-                                    <FontAwesomeIcon icon={faCalendar} width={32} height={32}
-                                                     className="text-blue-800"/>
-                                </div>
-                                <div className="text-md text-blue-default font-semibold opacity-60">
-                                    {formatDate(policy?.start_date)}
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    {/* <div className="mt-8 text-center">
-                        <a href="#" className="text-gray-500 underline underline-offset-[3px]">
-                            Qu'est-ce qui est couvert par mon contrat ?
-                        </a>
-                    </div> */}
-
-
-                </Card>
-
-                <div className="flex flex-col gap-8 items-start justify-start w-full p-4 flex-1">
-                    {(quickLinks ?? []).map((quickLink, idx) => {
-                        return <div onClick={() => handleQuickLinkClick(quickLink)} key={idx}
-                                    className={`pb-5 w-full flex items-center gap-2 justify-between text-2xl ${idx === quickLinks.length - 1 ? '' : 'border-b-2  border-gray-200'} hover:opacity-80 cursor-pointer`}>
-                            <div className="text-lg flex justify-center items-center gap-4 font-medium">
-                                <FontAwesomeIcon icon={quickLink.icon} width={32} height={32} className=""/>
-                                <span>
-                                    {quickLink.name}
-                                </span>
-                            </div>
-                            <FontAwesomeIcon icon={faChevronRight} width={18} height={18} className="text-gray-400"/>
-                        </div>
-                    })}
                 </div>
-            </div>
+
+            </> : <Alert variant="info" clickable={true} onClick={() => {
+                router.push(`${config.app.subscriptionUrl}/recapitulatif/${policy?.quote_id}`);
+            }}>
+                <div className="flex items-center gap-1">
+                    Vous n'avez pas terminé la souscription de votre contrat. Cliquez ici pour la finaliser. <FontAwesomeIcon icon={faChevronRight} width={18} height={18} className=""/>
+                </div>
+            </Alert>}
 
 
         </Page>
